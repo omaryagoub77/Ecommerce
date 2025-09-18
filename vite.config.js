5349df2455673315c54238b040be85d6d7b49528
Omar Yagoub <omaryagoub96@gmail.com>
2025-09-18 11:18:01 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Shop/', // <- MUST match your repo name
+  base: '/Ecommerce/', // <- MUST match your repo name
 })
